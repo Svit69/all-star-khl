95e7fbbef574c47a6e40118aaecf063e0816b6f5
Svit69 <ol.svitenkov@gmail.com>
2026-01-11 15:02:21 +0500

    Lock scroll and zoom during tutorial

--- src/services/TutorialController.js
+++ src/services/TutorialController.js
@@ -2,13 +2,15 @@ import { TutorialOverlay } from '../components/TutorialOverlay.js';
 import { TutorialStepRunner } from './TutorialStepRunner.js';
 
 export class TutorialController {
-  #steps; #index = 0; #overlay; #runner; #updateLayout;
+  #steps; #index = 0; #overlay; #runner; #updateLayout; #blockScroll; #blockZoom;
 
   constructor(steps) {
     this.#steps = steps;
     this.#overlay = new TutorialOverlay();
     this.#runner = new TutorialStepRunner(this.#overlay, this.#steps, () => this.next(), () => this.skip());
     this.#updateLayout = () => this.#runner.runStep(this.#index);
+    this.#blockScroll = (event) => event.preventDefault();
+    this.#blockZoom = (event) => event.preventDefault();
   }
 
   mount(parent) {
@@ -19,6 +21,7 @@ export class TutorialController {
   start() {
     if (!this.#steps.length) return;
     this.#overlay.setVisible(true);
+    this.#lockInteraction();
     this.#runner.runStep(this.#index);
     window.addEventListener('resize', this.#updateLayout);
     window.addEventListener('scroll', this.#updateLayout, true);
@@ -32,8 +35,27 @@ export class TutorialController {
 
   skip() {
     this.#overlay.setVisible(false);
+    this.#unlockInteraction();
     window.removeEventListener('resize', this.#updateLayout);
     window.removeEventListener('scroll', this.#updateLayout, true);
+  }
+
+  #lockInteraction() {
+    document.body.classList.add('tutorial-lock');
+    document.addEventListener('wheel', this.#blockScroll, { passive: false });
+    document.addEventListener('touchmove', this.#blockScroll, { passive: false });
+    document.addEventListener('gesturestart', this.#blockZoom, { passive: false });
+    document.addEventListener('gesturechange', this.#blockZoom, { passive: false });
+    document.addEventListener('gestureend', this.#blockZoom, { passive: false });
+  }
+
+  #unlockInteraction() {
+    document.body.classList.remove('tutorial-lock');
+    document.removeEventListener('wheel', this.#blockScroll);
+    document.removeEventListener('touchmove', this.#blockScroll);
+    document.removeEventListener('gesturestart', this.#blockZoom);
+    document.removeEventListener('gesturechange', this.#blockZoom);
+    document.removeEventListener('gestureend', this.#blockZoom);
   }
 
 }
